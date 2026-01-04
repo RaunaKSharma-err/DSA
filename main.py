8b0088e507b6088d8b8b4e77d914a0c23ee4a789
RaunaKSharma-err <progritik@gmail.com>
2026-01-04 14:34:54 +0545

    rotate matrix solution

---
+++ main.py
@@ -0,0 +1,12 @@
+class solution:
+    def rotateMatrix(self, matrix):
+        for i in range(0, len(matrix)):
+            for j in range(i + 1, len(matrix[0])):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            matrix[i].reverse()
+
+
+matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+c = solution()
+c.rotateMatrix(matrix)
+print(matrix)

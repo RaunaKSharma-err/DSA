5ceea557c119d39c2068857dab63ab9387545fd3
RaunaKSharma-err <progritik@gmail.com>
2026-01-06 12:11:35 +0545

    spiral matrix solution

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 class solution:
     def spiralMatrix(self, matrix):
         result = []
-        i = j = 0
-        while i < len(matrix):
-            result.append(matrix[i][j])
-            if j < len(matrix[0]) - 1:
-                j += 1
-            else:
-                i += 1
+        top = left = 0
+        bottom, right = len(matrix) - 1, len(matrix[0]) - 1
 
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
         print(result)
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
-s = ""
-print(len(s))
+for i in range(5, 0, -1):
+    print(i)

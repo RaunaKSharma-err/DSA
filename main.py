54d367f3c31428aa2df6577f892579aea1cf5ab4
RaunaKSharma-err <progritik@gmail.com>
2026-01-08 15:39:01 +0545

    foursum solution

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 class solution:
-    def threeSum(self, nums, target):
+    def fourSum(self, nums, target):
+        n = len(nums)
+        nums.sort()
         result = []
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    for l in range(k + 1, len(nums)):
-                        if (
-                            nums[i] + nums[j] + nums[k] + nums[l] == target
-                            and i != j
-                            and i != k
-                            and i != l
-                            and j != k
-                            and j != l
-                            and k != l
-                        ):
-                            temp = [nums[i], nums[j], nums[k], nums[l]]
-                            temp.sort()
-                            if temp not in result:
-                                result.append(temp)
+        for i in range(0, n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                k = j + 1
+                l = n - 1
+                while k < l:
+                    totalsum = nums[i] + nums[j] + nums[k] + nums[l]
+                    if totalsum == target:
+                        result.append([nums[i], nums[j], nums[k], nums[l]])
+                        k += 1
+                        l -= 1
+                        while k < l and nums[k] == nums[k - 1]:
+                            k += 1
+                        while l > k and nums[l] == nums[l + 1]:
+                            l -= 1
+                    elif totalsum < target:
+                        k += 1
+                    else:
+                        l -= 1
         return result
 
 
-m = [2, 2, 2, 2, 2]
+m = [1, 0, -1, 0, -2, 2]
 c = solution()
-x = c.threeSum(m, 8)
+x = c.threeSum(m, 0)
 print(x)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
-n = 5
-for i in range(n):
-    print(i)
+
+                    while l > k and nums[l] == nums[l + 1]:

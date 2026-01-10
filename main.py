f0b9c1b3f8d55a24f1d31494189c7d7df2994d5f
RaunaKSharma-err <progritik@gmail.com>
2026-01-10 20:37:10 +0545

    find the target in the rotated array problem

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 class solution:
-    def binaarySearch(self, nums, target):
+    def cielthefloor(self, nums, target):
         low = 0
         high = len(nums) - 1
-        if nums[high] < target:
-            return len(nums)
-        if nums[low] > target:
-            return 0
-        while low <= high:
-            mid = int((low + high) / 2)
-            if nums[mid] >= target:
-                high = mid - 1
-            else:
-                low = mid + 1
-        return low
+        if target <= nums[len(nums) - 1]:
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < nums[high]:
+                    if nums[mid] < target < nums[high]:
+                        low = mid + 1
+                    else:
+                        high = mid - 1
+                elif nums[low] < nums[mid]:
+                    if nums[low] < target < nums[mid]:
+                        high = mid - 1
+                    else:
+                        low = mid + 1
+        return -1
 
 
-nums1 = [1, 2, 3, 5, 6, 7, 8, 9, 11, 12, 13]
+nums = [11, 15, 20, 1, 4, 5, 6, 8, 9, 10]
 c = solution()
-x = c.binaarySearch(nums1, 4)
+x = c.cielthefloor(nums, 20)
 print(x)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-
+a = 2
+if a != 0:
+    if a > 1:
+        print("a is greater than 1")
+    else:
+        print("a is less than 1")
+print("something")

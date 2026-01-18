47aa0948b9d2907daa995cdb194ccd64b6de3d61
RaunaKSharma-err <progritik@gmail.com>
2026-01-18 20:05:52 +0545

    solution

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ class solution:
     def findTheElmentInTheRotatedArray(self, nums, target):
         low = 0
         high = len(nums) - 1
-        if target <= nums[len(nums) - 1]:
-            while low <= high:
-                mid = (low + high) // 2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] < nums[high]:
-                    if nums[mid] < target < nums[high]:
-                        low = mid + 1
-                    else:
-                        high = mid - 1
-                elif nums[low] < nums[mid]:
-                    if nums[low] < target < nums[mid]:
-                        high = mid - 1
-                    else:
-                        low = mid + 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return True
+            if nums[low] == nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+            if nums[mid] <= target:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            else:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
         return -1
 
 
-nums = [10, 11, 12, 13, 1, 2, 3, 4]
+nums = [1, 0, 1, 1, 1]
 c = solution()
-x = c.findTheElmentInTheRotatedArray(nums, 13)
+x = c.findTheElmentInTheRotatedArray(nums, 0)
 print(x)

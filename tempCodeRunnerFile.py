412614ce87f1c61e406b959f2d1a15219abfed3a
RaunaKSharma-err <progritik@gmail.com>
2026-04-09 20:49:11 +0545

    init

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,26 +1,3 @@
-
-# generate all subsequence using recursion
-def solveSubSequence(index, subset, total):
-    if target == total:
-        result.append(subset.copy())
-        return
-    elif target < total:
-        return
-    if index >= len(nums):
-        return
-
-    subset.append(nums[index])
-    sum = total + nums[index]
-    solveSubSequence(index + 1, subset, sum)
-    e = subset.pop()
-    sum -= e
-    solveSubSequence(index + 1, subset, sum)
-
-
-nums = [5, 9, 3, 4, 1]
 subset = []
-result = []
-target = 9
-total = 0
-solveSubSequence(0, subset, total)
-print(result)
+if sum(subset) < 2:
+    print("hey")

74189716ead1e7055de927e5567b5a53fa2a8f2b
RaunaKSharma-err <progritik@gmail.com>
2026-04-13 14:00:34 +0545

    generate all the binary string solution

--- main.py
+++ main.py
@@ -100,3 +100,23 @@ result = []
 total = 0
 generateParanthesis(0, total)
 print(result)
+
+# generate all the binary string
+subset = ["0"] * 3
+result = []
+
+
+def generateAllBinaryString(index, flag, subset, result):
+    if index >= len(subset):
+        result.append("".join(subset))
+        return
+    subset[index] = "0"
+    generateAllBinaryString(index + 1, True, subset, result)
+    if flag == True:
+        subset[index] = "1"
+        generateAllBinaryString(index + 1, False, subset, result)
+        subset[index] = "0"
+
+
+generateAllBinaryString(0, True, subset, result)
+print(result)

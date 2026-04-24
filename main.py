6ec28a941090c193a12174a2c0ba6bedf2a831cd
RaunaKSharma-err <progritik@gmail.com>
2026-04-24 20:05:17 +0545

    n-Queen leetcode problems

--- main.py
+++ main.py
@@ -281,3 +281,37 @@ result = []
 nums = [5, 9, 3]
 findSubsetSum(0, 0)
 print(result)
+
+# N - Queens leetcode problem
+n = 4
+ans = []
+board = ["." * n for _ in range(n)]
+leftRow = [0] * n
+upperDiagonal = [0] * (2 * n - 1)
+lowerDiagonal = [0] * (2 * n - 1)
+
+
+def findNQueen(col, n, board, ans, leftRow, upperDiagonal, lowerDiagonal):
+    if col == n:
+        ans.append(board[:])
+        return
+    for row in range(n):
+        if (
+            leftRow[row] == 0
+            and lowerDiagonal[row + col] == 0
+            and upperDiagonal[n - 1 + col - row] == 0
+        ):
+            board[row] = board[row][:col] + "Q" + board[row][col + 1 :]
+            leftRow[row] = 1
+            lowerDiagonal[row + col] = 1
+            upperDiagonal[n - 1 + col - row] = 1
+            findNQueen(col + 1, n, board, ans, leftRow, upperDiagonal, lowerDiagonal)
+            board[row] = board[row][:col] + "." + board[row][col + 1 :]
+            leftRow[row] = 0
+            lowerDiagonal[row + col] = 0
+            upperDiagonal[n - 1 + col - row] = 0
+
+
+findNQueen(0, n, board, ans, leftRow, upperDiagonal, lowerDiagonal)
+print(ans)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,32 @@
+# N - Queens leetcode problem
+n = 4
+ans = []
+board = ["." * n for _ in range(n)]
+leftRow = [0] * n
+upperDiagonal = [0] * (2 * n - 1)
+lowerDiagonal = [0] * (2 * n - 1)
 
-def findSubsetSum(index, total):
-    if index >= len(nums):
-        result.append(total)
+
+def findNQueen(col, n, board, ans, leftRow, upperDiagonal, lowerDiagonal):
+    if col == n:
+        ans.append(board[:])
         return
-    sums = total + nums[index]
-    findSubsetSum(index + 1, sums)
-    findSubsetSum(index + 1, total)
+    for row in range(n):
+        if (
+            leftRow[row] == 0
+            and lowerDiagonal[row + col] == 0
+            and upperDiagonal[n - 1 + col - row] == 0
+        ):
+            board[row] = board[row][:col] + "Q" + board[row][col + 1 :]
+            leftRow[row] = 1
+            lowerDiagonal[row + col] = 1
+            upperDiagonal[n - 1 + col - row] = 1
+            findNQueen(col + 1, n, board, ans, leftRow, upperDiagonal, lowerDiagonal)
+            board[row] = board[row][:col] + "." + board[row][col + 1 :]
+            leftRow[row] = 0
+            lowerDiagonal[row + col] = 0
+            upperDiagonal[n - 1 + col - row] = 0
 
 
-result = []
-nums = [5, 9, 3]
-findSubsetSum(0, 0)
-print(result)
+findNQueen(0, n, board, ans, leftRow, upperDiagonal, lowerDiagonal)
+print(ans)

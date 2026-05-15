46fa31b689cf7aea16da292cc64cc936515c0975
RaunaKSharma-err <progritik@gmail.com>
2026-05-15 14:03:04 +0545

    asteroid collision solution

--- Stack.py
+++ Stack.py
@@ -160,3 +160,22 @@ nums = [19, 2, 4, 9, 3, 5, 8, 10]
 ans = [-1] * len(nums)
 nextGreaterElement(nums)
 print(ans)
+
+# next greatear element|| solution
+
+def nextGreaterElement(nums):
+    stack = []
+    n = len(nums)
+    for i in range(2*n - 1, -1, -1):
+        while len(stack) > 0 and nums[i%n] > stack[-1]:
+            stack.pop()
+        if i <n:
+            if len(stack) != 0:
+                ans[i]=stack[-1]
+        stack.append(nums[i%n])
+
+
+nums = [19, 2, 4, 9, 3, 5, 8, 10]
+ans = [-1] * len(nums)
+nextGreaterElement(nums)
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,13 @@
 def nextGreaterElement(nums):
     stack = []
     n = len(nums)
-    for i in range(n - 1, 0, -1):
-        while len(stack) > 0 and nums[i] > stack[-1]:
+    for i in range(2*n - 1, -1, -1):
+        while len(stack) > 0 and nums[i%n] > stack[-1]:
             stack.pop()
-        if len(stack) != 0:
-            ans[i]=stack[-1]
-        stack.append(nums[i])
+        if i <n:
+            if len(stack) != 0:
+                ans[i]=stack[-1]
+        stack.append(nums[i%n])
 
 
 nums = [19, 2, 4, 9, 3, 5, 8, 10]

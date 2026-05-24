0099790e2b6ce5d748a86783ba0fbd66ed5e3e62
RaunaKSharma-err <progritik@gmail.com>
2026-05-24 14:18:24 +0545

    minimum number of coin

--- Stack.py
+++ Stack.py
@@ -320,3 +320,25 @@ g = [2, 6, 8, 1, 4]
 s = [4, 2, 7, 1, 2, 3]
 ans = findContentChildren(g, s)
 print(ans)
+
+
+# Minimum number of coins leetcode solution
+
+
+def coinChange(coins, amount):
+    coinSet = []
+    coins.sort(reverse=True)
+    for coin in coins:
+        while amount >= coin:
+            amount -= coin
+            coinSet.append(coin)
+
+    if amount == 0:
+        return coinSet
+    return []
+
+
+coins = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
+amount = 43
+ans = coinChange(coins, amount)
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,17 @@
-def findContentChildren(g, s):
-    g.sort()
-    s.sort()
-    count = 0
-    left = 0
-    right = 0
-    while left < len(g) and right < len(s):
-        if g[left] <= s[right]:
-            count += 1
-            left += 1
-            right += 1
-        else:
-            right += 1
-    return count
+def coinChange(coins, amount):
+    coinSet = []
+    coins.sort(reverse=True)
+    for coin in coins:
+        while amount >= coin:
+            amount -= coin
+            coinSet.append(coin)
+
+    if amount == 0:
+        return coinSet
+    return []
 
 
-g = [2, 6, 8, 1, 4]
-s = [4, 2, 7, 1, 2, 3]
-ans = findContentChildren(g, s)
+coins = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
+amount = 43
+ans = coinChange(coins, amount)
 print(ans)

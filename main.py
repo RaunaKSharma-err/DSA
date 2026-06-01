0b6cfc1b360051cc8bf9244556b95dfe14d5394a
RaunaKSharma-err <progritik@gmail.com>
2026-06-01 10:01:17 +0545

    top view of the binary tree

--- main.py
+++ main.py
@@ -565,7 +565,8 @@ print(ans)
 
 
 def MaximumPathSum(root):
-    maxi=0
+    maxi = 0
+
     def solve(node):
         if node is None:
             return 0
@@ -577,5 +578,45 @@ def MaximumPathSum(root):
             rightSum = 0
         maxi = max(maxi, leftSum + rightSum + node.val)
         return max(leftSum, rightSum) + node.val
+
     solve(root)
     return maxi
+
+
+# top view of the binary tree
+
+
+class node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+root = node(5)
+root.left = node(3)
+root.right = node(1)
+root.right.left = node(8)
+root.right.right = node(7)
+root.left.left = node(6)
+root.left.right = node(2)
+
+
+def topView(node):
+    if node is None:
+        return None
+    queue = [(node, 0)]
+    top_view = {}
+    while queue:
+        current_node, line = queue.pop(0)
+        if line not in top_view:
+            top_view[line] = current_node.val
+        if current_node.left:
+            queue.append((current_node.left, line - 1))
+        if current_node.right:
+            queue.append((current_node.right, line + 1))
+    return [top_view[key] for key in sorted(top_view.keys())]
+
+
+ans = topView(root)
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,22 +1,34 @@
-def minimumPlatform(arr, dep):
-    arr.sort()
-    dep.sort()
-    platform_needed = 1
-    max_platforms = 1
-    j = 0
-    i = 1
-    while i < len(arr) and j < len(dep):
-        if arr[i] <= dep[j]:
-            platform_needed += 1
-            i += 1
-        else:
-            platform_needed -= 1
-            j += 1
-        max_platforms = max(max_platforms, platform_needed)
-    return max_platforms
+class node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
 
-ans = minimumPlatform(
-    [900, 940, 950, 1100, 1500, 1800], [910, 1200, 1120, 1130, 1900, 2000]
-)
+root = node(5)
+root.left = node(3)
+root.right = node(1)
+root.right.left = node(8)
+root.right.right = node(7)
+root.left.left = node(6)
+root.left.right = node(2)
+
+
+def topView(node):
+    if node is None:
+        return None
+    queue = [(node, 0)]
+    top_view = {}
+    while queue:
+        current_node, line = queue.pop(0)
+        if line not in top_view:
+            top_view[line] = current_node.val
+        if current_node.left:
+            queue.append((current_node.left, line - 1))
+        if current_node.right:
+            queue.append((current_node.right, line + 1))
+    return [top_view[key] for key in sorted(top_view.keys())]
+
+
+ans = topView(root)
 print(ans)

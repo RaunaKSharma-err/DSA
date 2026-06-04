472d19f7d68ffd0c8d36547cf5dcfafb70aff84d
RaunaKSharma-err <progritik@gmail.com>
2026-06-04 18:17:50 +0545

    BFS

--- graph.py
+++ graph.py
@@ -33,3 +33,27 @@ adjacency_list = [
 ]
 ans = bfs(n, adjacency_list, 2)
 print(ans)
+
+# DFS
+
+
+def dfs(n, adjacency_list, node):
+    visited = [False] * (n + 1)
+    ans = []
+
+    def dfs(node):
+        visited[node] = True
+        ans.append(node)
+        for neighbor in adjacency_list[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+
+    dfs(node)
+    return ans
+
+
+n = 8
+adjacency_list = [[], [2, 4], [1, 3, 6], [2], [1, 5, 7], [4, 8], [2], [4, 8], [5, 7]]
+ans = dfs(n, adjacency_list, 1)
+print(ans)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,38 +1,20 @@
-from collections import deque
 
-
-def bfs(n, adjacency_list):
-    queue = deque()
+def dfs(n, adjacency_list, node):
     visited = [False] * (n + 1)
-    ans =[]
-    for i in range(1, n + 1):
-        for j in adjacency_list[i]:
-            if not visited[j]:
-                queue.append(j)
-                visited[j] = True
-                while queue:
-                    node = queue.popleft()
-                    ans.append(node)
-                    for neighbor in adjacency_list[node]:
-                        if not visited[neighbor]:
-                            queue.append(neighbor)
-                            visited[neighbor] = True
+    ans = []
+
+    def dfs(node):
+        visited[node] = True
+        ans.append(node)
+        for neighbor in adjacency_list[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+
+    dfs(node)
     return ans
-                
 
 
-n = 9
-adjacency_list = [
-    [],
-    [2, 8],
-    [1, 3, 4],
-    [2],
-    [2, 5],
-    [4, 6],
-    [5, 7],
-    [6, 8],
-    [1, 7, 9],
-    [8],
-]
-ans = bfs(n, adjacency_list)
+n = 8
+adjacency_list = [[], [2, 4], [1, 3, 6], [2], [1, 5, 7], [4, 8], [2], [4, 8], [5, 7]]
+ans = dfs(n, adjacency_list, 1)
 print(ans)

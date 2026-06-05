9f8e90c4619134d971d978c9f280d528376c658c
RaunaKSharma-err <progritik@gmail.com>
2026-06-05 12:08:11 +0545

    cycle detection in undirected graph

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-
 def bfs(n, adjacency_list, starting_node):
     ans = []
     queue = deque()
@@ -57,3 +56,96 @@ adjacency_list = [[], [2, 4], [1, 3, 6], [2], [1, 5, 7], [4, 8], [2], [4, 8], [5
 ans = dfs(n, adjacency_list, 1)
 print(ans)
 
+# Rotting oranges leetcode problem
+
+
+def orangesRotting(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    queue = deque()
+    fresh_oranges = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 2:
+                queue.append((i, j))
+            elif grid[i][j] == 1:
+                fresh_oranges += 1
+
+    minutes = 0
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    while queue and fresh_oranges > 0:
+        minutes += 1
+        for _ in range(len(queue)):
+            x, y = queue.popleft()
+            for dx, dy in directions:
+                new_x, new_y = x + dx, y + dy
+                if (
+                    0 <= new_x < rows
+                    and 0 <= new_y < cols
+                    and grid[new_x][new_y] == 1
+                ):
+                    grid[new_x][new_y] = 2
+                    fresh_oranges -= 1
+                    queue.append((new_x, new_y))
+
+    return minutes if fresh_oranges == 0 else -1
+
+ans = orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]])
+print(ans)
+   
+#flood fill leetcode problem
+from collections import deque
+from copy import deepcopy
+
+def floodFill(image, sr, sc, newColor):
+    rows = len(image)
+    cols = len(image[0])
+    visited = deepcopy(image)
+    queue = deque()
+    queue.append((sr, sc))
+    if image[sr][sc] == newColor:
+        return image
+    while queue:
+        x,y = queue.popleft()
+        visited[x][y] = newColor
+        for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+            new_x, new_y = x + dx, y + dy
+            if 0 <= new_x < rows and 0 <= new_y < cols and visited[new_x][new_y] == image[sr][sc]:
+                queue.append((new_x, new_y))
+    return visited
+
+
+ans = floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2)
+print(ans)
+
+#detect cycle in undirected graph
+from collections import deque
+def has_cycle(v,e, adjacency_list):
+    visited = [False] * (v + 1)
+    queue = deque()
+    queue.append((1, -1))
+    visited[1] = True
+    while queue:
+        node, parent = queue.popleft()
+        for neighbor in adjacency_list[node]:
+            if not visited[neighbor]:
+                visited[neighbor]=True
+                queue.append((neighbor, node))
+            elif neighbor != parent:
+                return True
+    return False   
+        
+adjacency_list = [
+    [],
+    [2,4],
+    [1, 3, 6],
+    [2],
+    [1, 5],
+    [4, 7],
+    [2, 7],
+    [5,6],
+]
+ans = has_cycle(7, 7,adjacency_list)
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,28 @@
-
-def dfs(n, adjacency_list, node):
-    visited = [False] * (n + 1)
-    ans = []
-
-    def dfs(node):
-        visited[node] = True
-        ans.append(node)
+from collections import deque
+def has_cycle(v,e, adjacency_list):
+    visited = [False] * (v + 1)
+    queue = deque()
+    queue.append((1, -1))
+    visited[1] = True
+    while queue:
+        node, parent = queue.popleft()
         for neighbor in adjacency_list[node]:
             if not visited[neighbor]:
-                dfs(neighbor)
-
-    dfs(node)
-    return ans
-
-
-n = 8
-adjacency_list = [[], [2, 4], [1, 3, 6], [2], [1, 5, 7], [4, 8], [2], [4, 8], [5, 7]]
-ans = dfs(n, adjacency_list, 1)
+                visited[neighbor]=True
+                queue.append((neighbor, node))
+            elif neighbor != parent:
+                return True
+    return False   
+        
+adjacency_list = [
+    [],
+    [2,4],
+    [1, 3, 6],
+    [2],
+    [1, 5],
+    [4, 7],
+    [2, 7],
+    [5,6],
+]
+ans = has_cycle(7, 7,adjacency_list)
 print(ans)

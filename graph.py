192b2e0a129ce401f7749ae52a188ec6ac31730a
RaunaKSharma-err <progritik@gmail.com>
2026-06-21 20:11:05 +0545

    courseSchedule 2

--- graph.py
+++ graph.py
@@ -534,9 +534,7 @@ def topoSort(adj_list):
             if indegrees[neighbour]-1==0:
                 queue.append(neighbour)
             indegrees[neighbour]-=1
-    if len(ans) == len(adj_list):
-        return False
-    return True
+    return ans
 
 adjacency_list = [
     [0,1],
@@ -545,4 +543,28 @@ adjacency_list = [
     [2,0]
 ]
 ans = topoSort(adjacency_list)
+print(ans)
+
+from collections import deque
+def CourseSchedule2(numCourses,prerequisites):
+    adj_list = [[] for _ in range(numCourses)]
+    indegrees = [0] * numCourses
+    for course, prereq in prerequisites:
+        adj_list[prereq].append(course)
+        indegrees[course] += 1
+    queue = deque()
+    for i in range(numCourses):
+        if indegrees[i] == 0:
+            queue.append(i)
+    ans = []
+    while queue:
+        node = queue.popleft()
+        ans.append(node)
+        for neighbour in adj_list[node]:
+            indegrees[neighbour] -= 1
+            if indegrees[neighbour] == 0:
+                queue.append(neighbour)
+    return ans if len(ans) == numCourses else []
+
+ans = CourseSchedule2(4,[[1,0],[2,0],[3,1],[3,2]])
 print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,28 +1,24 @@
+
 from collections import deque
-def topoSort(adj_list):
-    ans=[]
+def CourseSchedule2(numCourses,prerequisites):
+    adj_list = [[] for _ in range(numCourses)]
+    indegrees = [0] * numCourses
+    for course, prereq in prerequisites:
+        adj_list[prereq].append(course)
+        indegrees[course] += 1
     queue = deque()
-    indegrees = [0 for _ in range(len(adj_list))]
-    for u,v in adj_list:
-        adj_list[u].append(v)
-        indegrees[v]+=1
-    for i in range(0,len(indegrees)):
-        if indegrees[i]==0:
+    for i in range(numCourses):
+        if indegrees[i] == 0:
             queue.append(i)
+    ans=[]
     while queue:
         node = queue.popleft()
         ans.append(node)
         for neighbour in adj_list[node]:
-            if indegrees[neighbour]-1==0:
+            indegrees[neighbour] -= 1
+            if indegrees[neighbour] == 0:
                 queue.append(neighbour)
-            indegrees[neighbour]-=1
     return ans
 
-adjacency_list = [
-    [0,1],
-    [0,2],
-    [1,2],
-    [2,0]
-]
-ans = topoSort(adjacency_list)
+ans = CourseSchedule2(4,[[1,0],[2,0],[3,1],[3,2]])
 print(ans)

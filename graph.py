6443b4f0c32be05f7dc0a4a1cc9cb20347d1f955
RaunaKSharma-err <progritik@gmail.com>
2026-06-23 21:53:15 +0545

    Eventual Safe States leetcode solution

--- graph.py
+++ graph.py
@@ -568,3 +568,40 @@ def CourseSchedule2(numCourses,prerequisites):
 
 ans = CourseSchedule2(4,[[1,0],[2,0],[3,1],[3,2]])
 print(ans)
+
+# Eventual Safe States leetcode solution
+from collections import deque
+def safeNode(adjacency_list):
+    ans=[]
+    queue = deque()
+    adj_list = [[]for _ in range(len(adjacency_list))]
+    indegrees = [0 for _ in range(len(adj_list))]
+
+    for node in range(len(adjacency_list)):
+        for neighbor in adjacency_list[node]:
+            adj_list[neighbor].append(node)
+            indegrees[node]+=1
+            
+    for node in range(0,len(indegrees)):
+        if indegrees[node]==0:
+            queue.append(node)
+
+    while queue:
+        node = queue.popleft()
+        ans.append(node)
+        for neighbour in adj_list[node]:
+            indegrees[neighbour] -= 1
+            if indegrees[neighbour] == 0:
+                queue.append(neighbour)
+    return ans
+adjacency_list = [
+    [1,2],
+    [2,3],
+    [5],
+    [0],
+    [5],
+    [],
+    [],
+]
+ans = safeNode(adjacency_list)
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,19 @@
-
 from collections import deque
-def CourseSchedule2(numCourses,prerequisites):
-    adj_list = [[] for _ in range(numCourses)]
-    indegrees = [0] * numCourses
-    for course, prereq in prerequisites:
-        adj_list[prereq].append(course)
-        indegrees[course] += 1
-    queue = deque()
-    for i in range(numCourses):
-        if indegrees[i] == 0:
-            queue.append(i)
+def safeNode(adjacency_list):
     ans=[]
+    queue = deque()
+    adj_list = [[]for _ in range(len(adjacency_list))]
+    indegrees = [0 for _ in range(len(adj_list))]
+
+    for node in range(len(adjacency_list)):
+        for neighbor in adjacency_list[node]:
+            adj_list[neighbor].append(node)
+            indegrees[node]+=1
+            
+    for node in range(0,len(indegrees)):
+        if indegrees[node]==0:
+            queue.append(node)
+
     while queue:
         node = queue.popleft()
         ans.append(node)
@@ -19,6 +22,14 @@ def CourseSchedule2(numCourses,prerequisites):
             if indegrees[neighbour] == 0:
                 queue.append(neighbour)
     return ans
-
-ans = CourseSchedule2(4,[[1,0],[2,0],[3,1],[3,2]])
+adjacency_list = [
+    [1,2],
+    [2,3],
+    [5],
+    [0],
+    [5],
+    [],
+    [],
+]
+ans = safeNode(adjacency_list)
 print(ans)

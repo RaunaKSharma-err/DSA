8f9d224370781bce79a137bff200f376b3b7c58a
RaunaKSharma-err <progritik@gmail.com>
2026-07-12 21:08:11 +0545

    dijkstra shortest path solution

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,26 +1,37 @@
-from collections import deque
-def shortestPath(src, adjList):
-    distance = [-1]*len(adjList)
-    distance[src] = 0
-    queue = deque([(src,0)])
+import heapq
+def dijkstra(edges, src):
+    adjList = [[] for _ in range(len(edges))]
+    for u,v,w in edges:
+        adjList[u].append([v,w])
+    distance = ["inf"]*len(edges)
+    queue = [[src,0]]
     while queue:
-        node,dist = queue.popleft()
-        for neighbor in adjList[node]:
-            if distance[neighbor]==-1:
-                distance[neighbor] = dist+1
-                queue.append((neighbor,dist+1))
+        node,dist = heapq.heappop(queue)
+        if distance[node]!="inf":
+            continue
+        distance[node] = dist
+        for neighbor,weight in adjList[node]:
+            if distance[neighbor]=="inf":
+                heapq.heappush(queue,(neighbor,dist+weight))
     return distance
 
-adjList= [
-    [1,2],
+edges= [
+    [0,1,4],
     [0,2,4],
-    [0,1,3],
-    [2,5],
-    [1,6],
-    [3,6],
-    [4,5,7,8],
-    [6,8],
-    [6,7]
+    [1,0,4],
+    [1,2,2],
+    [2,0,4],
+    [2,1,2],
+    [2,3,3],
+    [2,4,1],
+    [2,5,6],
+    [3,2,3],
+    [3,5,2],
+    [4,2,1],
+    [4,5,3],
+    [5,2,6],
+    [5,3,2],
+    [5,4,3]
 ]
-ans = shortestPath(0,adjList)
+ans = dijkstra(edges,0)
 print(ans)

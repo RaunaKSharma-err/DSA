e2cf3d575f5f1d7761c0ef8b7f1d406dc5b938c2
RaunaKSharma-err <progritik@gmail.com>
2026-07-13 15:56:06 +0545

    dijkstra using set

--- graph.py
+++ graph.py
@@ -722,7 +722,7 @@ adjList= [
 ans = shortestPath(0,adjList)
 print(ans)
 
-# dijkstra algorithm 
+# dijkstra algorithm using priority queue
 import heapq
 def dijkstra(edges, src):
     adjList = [[] for _ in range(len(edges))]
@@ -759,4 +759,46 @@ edges= [
     [5,4,3]
 ]
 ans = dijkstra(edges,0)
+print(ans)
+
+# dijkstra algorithm using set
+import sys
+def dijkstraSet(edges,src):
+    adjList = [[] for _ in range(len(edges))]
+    for u,v ,w in edges:
+        adjList[u].append([v,w])
+    distance = [sys.maxsize for _ in range(len(edges))]
+    distance[src] = 0
+    myset = set()
+    myset.add((0,src))
+    while len(myset) !=0:
+        dist,node = min(myset)
+        myset.discard((dist,node))
+        for neighbor ,weight in adjList[node]:
+            distanceTravel = dist+weight
+            if distanceTravel < distance[neighbor]:
+                if distance[neighbor]!=sys.maxsize:
+                    myset.discard((distance[neighbor],neighbor))
+                distance[neighbor] = distanceTravel
+                myset.add((distanceTravel,neighbor))
+    return distance
+edges= [
+    [0,1,4],
+    [0,2,4],
+    [1,0,4],
+    [1,2,2],
+    [2,0,4],
+    [2,1,2],
+    [2,3,3],
+    [2,4,1],
+    [2,5,6],
+    [3,2,3],
+    [3,5,2],
+    [4,2,1],
+    [4,5,3],
+    [5,2,6],
+    [5,3,2],
+    [5,4,3]
+]
+ans = dijkstraSet(edges,0)
 print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,23 @@
-import heapq
-def dijkstra(edges, src):
+import sys
+def dijkstraSet(edges,src):
     adjList = [[] for _ in range(len(edges))]
-    for u,v,w in edges:
+    for u,v ,w in edges:
         adjList[u].append([v,w])
-    distance = ["inf"]*len(edges)
-    queue = [[src,0]]
-    while queue:
-        node,dist = heapq.heappop(queue)
-        if distance[node]!="inf":
-            continue
-        distance[node] = dist
-        for neighbor,weight in adjList[node]:
-            if distance[neighbor]=="inf":
-                heapq.heappush(queue,(neighbor,dist+weight))
+    distance = [sys.maxsize for _ in range(len(edges))]
+    distance[src] = 0
+    myset = set()
+    myset.add((0,src))
+    while len(myset) !=0:
+        dist,node = min(myset)
+        myset.discard((dist,node))
+        for neighbor ,weight in adjList[node]:
+            distanceTravel = dist+weight
+            if distanceTravel < distance[neighbor]:
+                if distance[neighbor]!=sys.maxsize:
+                    myset.discard((distance[neighbor],neighbor))
+                distance[neighbor] = distanceTravel
+                myset.add((distanceTravel,neighbor))
     return distance
-
 edges= [
     [0,1,4],
     [0,2,4],
@@ -33,5 +36,5 @@ edges= [
     [5,3,2],
     [5,4,3]
 ]
-ans = dijkstra(edges,0)
+ans = dijkstraSet(edges,0)
 print(ans)

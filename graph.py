83e7821e754d9c24d69a347141abb86418e6c1db
RaunaKSharma-err <progritik@gmail.com>
2026-07-23 20:24:16 +0545

    number of ways to arrive at destination

--- graph.py
+++ graph.py
@@ -935,3 +935,31 @@ def findCheapestPrice(n, flights, src, dst, k):
 
 ans = findCheapestPrice(3,[[0,1,100],[1,2,100],[0,2,500]], 0, 2, 0)
 print(ans)
+
+# number of ways to arrive at destination
+
+import heapq,sys
+def shortestWays(n, roads):
+    MOD = 10**9+7
+    adj_list = [[] for _ in range(n)]
+    for u ,v ,w in roads:
+        adj_list[u].append([v,w])
+        adj_list[v].append([u,w])
+    distance = [sys.maxsize for _ in range(n)]
+    ways = [0 for _ in range(n)]
+    distance[0]=0
+    ways[0]=1
+    priority_queue = [[0,0]]
+    while priority_queue:
+        dist,node = heapq.heappop(priority_queue)
+        for neighbor,weight in adj_list[node]:
+            new_dist = dist+weight
+            if new_dist < distance[neighbor]:
+                distance[neighbor] = new_dist
+                heapq.heappush(priority_queue,[new_dist,neighbor])
+                ways[neighbor] = ways[node]
+            elif new_dist == distance[neighbor]:
+                ways[neighbor]=(ways[neighbor]+ways[node])%MOD
+    return ways[n-1]%MOD
+ans = shortestWays(7,[[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]])
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,23 +1,25 @@
-import heapq
-def findCheapestPrice(n, flights, src, dst, k):
+import heapq,sys
+def shortestWays(n, roads):
+    MOD = 10**9+7
     adj_list = [[] for _ in range(n)]
-    for u,v,w in flights:
+    for u ,v ,w in roads:
         adj_list[u].append([v,w])
-    heap = [(0, src, 0)]
-    dist = [[float('inf')] * (k + 2) for _ in range(n)]
-    dist[src][0] = 0
-    while heap:
-        cost, node, stops = heapq.heappop(heap)
-        if node == dst:
-            return cost
-        if stops == k + 1:
-            continue
-        for nei, price in adj_list[node]:
-            new_cost = cost + price
-            if new_cost < dist[nei][stops + 1]:
-                dist[nei][stops + 1] = new_cost
-                heapq.heappush(heap, (new_cost, nei, stops + 1))
-    return -1
-
-ans = findCheapestPrice(3,[[0,1,100],[1,2,100],[0,2,500]], 0, 2, 0)
+        adj_list[v].append([u,w])
+    distance = [sys.maxsize for _ in range(n)]
+    ways = [0 for _ in range(n)]
+    distance[0]=0
+    ways[0]=1
+    priority_queue = [[0,0]]
+    while priority_queue:
+        dist,node = heapq.heappop(priority_queue)
+        for neighbor,weight in adj_list[node]:
+            new_dist = dist+weight
+            if new_dist < distance[neighbor]:
+                distance[neighbor] = new_dist
+                heapq.heappush(priority_queue,[new_dist,neighbor])
+                ways[neighbor] = ways[node]
+            elif new_dist == distance[neighbor]:
+                ways[neighbor]=(ways[neighbor]+ways[node])%MOD
+    return ways[n-1]%MOD
+ans = shortestWays(7,[[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]])
 print(ans)

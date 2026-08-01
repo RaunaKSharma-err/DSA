8dae18f6eaf5c751d0590f0c01c7c92e8a6cdeca
RaunaKSharma-err <progritik@gmail.com>
2026-08-01 21:30:34 +0545

    init

--- graph.py
+++ graph.py
@@ -968,3 +968,13 @@ def maxProduct(nums):
     nums.sort()
     return (nums[-1]-1)*(nums[-2]-1)
 print(maxProduct([1,5,4,5]))
+
+hashmap = {'a':1,'b':2,'c':3,'d':1,'e':2,'f':3,'g':1,'h':2,'i':3,'j':1,'k':2,'l':3,'m':1,'n':2,'o':3,'p':1,'q':2,'r':3,'s':4,'t':1,'u':2,'v':3,'w':1,'x':2,'y':3,'z':4}
+def minimumPushes(val):
+    count=0
+    for ch in val:
+        if ch in hashmap:
+            count += hashmap[ch]
+    return count
+ans = minimumPushes("xycdefghij")
+print(ans)

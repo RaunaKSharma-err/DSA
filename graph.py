6a8418c06dbb2781b6d620291d565d893d1083fa
RaunaKSharma-err <progritik@gmail.com>
2026-08-07 19:42:43 +0545

    insert into binary search tree leetcode solution

--- graph.py
+++ graph.py
@@ -1004,3 +1004,66 @@ def findCeilInBst(root,ceil):
 ans = findCeilInBst([],9)
 print(ans)
 
+#find the floor in the bst
+
+def findTheFloorInBst(root,floor):
+    ans=-1
+    while root :
+        if root.val == floor:
+            return root.val
+        elif root.val > floor:
+            root = root.left
+        else:
+            root = root.right
+            ans = root.val
+    return ans
+
+ans = findTheFloorInBst([],9)
+print(ans)
+
+#Insert into a Binary Search Tree leetcode solution
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def insertIntoBST(root,node):
+    if not root:
+        return TreeNode(node)
+    if root.val < node:
+        newnode = TreeNode(node)
+        newnode.left = root
+        newnode.right = root.right
+        root.right = newnode
+        root = newnode
+    else:
+        newnode = TreeNode(node)
+        newnode.right = root
+        newnode.left = root.left
+        root.left = newnode
+        root = newnode
+    return root
+
+ans = insertIntoBST(TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7)), 5)
+print(ans)
+
+def insertIntoBst(root,val):
+    temp = root 
+    if not root:
+        return TreeNode(val)
+    while temp:
+        if temp.val < val:
+            if temp.right is None:
+                temp.right = TreeNode(val)
+                break
+            temp = temp.right
+        else:
+            if temp.left is None:
+                temp.left = TreeNode(val)
+                break
+            temp = temp.left
+    return root
+
+ans = insertIntoBst(TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7)), 5)
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,44 @@
-def findMinimumBst(root):
-    while root.left.left:
-        root = root.left
-    return root.left.val - root.left.left.val
-ans = findMinimumBst([4,2,6,1,3])
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# def insertIntoBST(root,node):
+#     if not root:
+#         return TreeNode(node)
+#     if root.val < node:
+#         newnode = TreeNode(node)
+#         newnode.left = root
+#         newnode.right = root.right
+#         root.right = newnode
+#         root = newnode
+#     else:
+#         newnode = TreeNode(node)
+#         newnode.right = root
+#         newnode.left = root.left
+#         root.left = newnode
+#         root = newnode
+#     return root
+
+# ans = insertIntoBST(TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7)), 5)
+# print(ans)
+
+def insertIntoBst(root,node):
+    temp = root 
+    newnode = TreeNode(node)
+    while temp:
+        if temp.val < node:
+            if temp.right is None:
+                temp.right = newnode
+                break
+            temp = temp.right
+        else:
+            if temp.left is None:
+                temp.left = newnode
+                break
+            temp = temp.left
+    return root
+
+ans = insertIntoBst(TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7)), 5)
 print(ans)

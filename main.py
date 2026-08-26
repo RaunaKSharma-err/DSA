d29d52e06522c9f713d92a31bd428917ad5bc26c
RaunaKSharma-err <progritik@gmail.com>
2026-08-26 12:43:11 +0545

    return the first subsequence if the sum with the target exists

--- main.py
+++ main.py
@@ -688,3 +688,24 @@ target=9
 solve([],0,0)
 print(result)
 
+def solve(subseq,index,total):
+    if index >= len(lst):
+        if target == total:
+            result.append(subseq.copy())
+            return  True
+        return
+    subseq.append(lst[index])
+    total += lst[index]
+    if solve(subseq,index+1,total):
+        return True
+    val = subseq.pop()
+    total -= val
+    if solve(subseq,index+1,total):
+        return True
+    return  False
+
+lst = [5,9,3,4,1]
+target=4
+result=[]
+ans = solve([],0,0)
+print(ans,result)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,17 +2,20 @@ def solve(subseq,index,total):
     if index >= len(lst):
         if target == total:
             result.append(subseq.copy())
+            return  True
         return
     subseq.append(lst[index])
     total += lst[index]
-    solve(subseq,index+1,total)
+    if solve(subseq,index+1,total):
+        return True
     val = subseq.pop()
     total -= val
-    solve(subseq,index+1,total)
-    return
+    if solve(subseq,index+1,total):
+        return True
+    return  False
 
 lst = [5,9,3,4,1]
-result = []
-target=9
-solve([],0,0)
-print(result)
+target=4
+result=[]
+ans = solve([],0,0)
+print(ans,result)

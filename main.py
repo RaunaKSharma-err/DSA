8dad9394d6ba04fd1fba05c8b38279a5577dbe5c
RaunaKSharma-err <progritik@gmail.com>
2026-08-29 20:32:52 +0545

    is power of three leetcode solution

--- main.py
+++ main.py
@@ -776,3 +776,15 @@ target=7
 result=[]
 backtrack([],0,7)
 print(result)
+
+def isPowerOfThree(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+    if n % 3 != 0:
+        return False
+    return isPowerOfThree(n // 3)
+
+ans = isPowerOfThree(27)
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,18 +1,11 @@
-def backtrack(res,index,sum):
-    if index >=len(nums) or sum < 0:
-        return
-    if sum == 0:
-        result.append(res.copy())
-        return
-    res.append(nums[index])
-    sum -= nums[index]
-    backtrack(res,index,sum)
-    sum += nums[index]
-    res.pop()
-    backtrack(res,index+1,sum) 
+def isPowerOfThree(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+    if n % 3 != 0:
+        return False
+    return isPowerOfThree(n // 3)
 
-nums = [2,3,6,7]
-target=7
-result=[]
-backtrack([],0,7)
-print(result)
+ans = isPowerOfThree(27)
+print(ans)

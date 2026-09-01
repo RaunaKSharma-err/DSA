63dc82b5151c37fbd4709432fc18f66b0edb1c72
RaunaKSharma-err <progritik@gmail.com>
2026-09-01 20:35:09 +0545

    init

--- main.py
+++ main.py
@@ -795,5 +795,22 @@ def fib(n):
     if n==0:
         return 0
     return fib(n-1)+fib(n-2)
+
 ans = fib(8)
 print(ans)
+
+def solve(val,index):
+    if index >= len(lst) or len(val)>3:
+        return
+    if len(val) == 3 and int(val)%2==0:
+        result.append(int(val.copy()))
+        return
+    val += str(lst[index])
+    solve(val,index+1)
+    val = val[:-1]
+    solve(val,index+1)
+
+lst = [2,1,3,0]
+result = []
+solve("",0)
+print(result)

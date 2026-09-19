a7f068d32c8a140a54e997f1c104a2edecc4f4a8
RaunaKSharma-err <progritik@gmail.com>
2026-09-19 18:40:47 +0545

    minimum path sum in triangle

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -102,22 +102,36 @@ def minimumPathSum(m,n,grid):
         prev = curr.copy()
         return prev[n-1]
 
-def minimumPathSum(m,n,grid):
-    prev = [0]*n
-    for i in range(m):
-        curr = [0]*n
-        for j in range(n):
-            if i ==0 and j==0:
-                curr[0] = grid[0][0]
-                continue
-            if i==0:
-                up = float("inf")
-            else:
-                up = prev[j]
-            if j==0:
-                diagonal = float("inf")
-            else:
-                diagonal = curr[j-1]
-            curr[j] = grid[i][j]+min(up,diagonal)
-        prev = curr.copy()
-        return prev[n-1]
+def minimumPathSumInTriangular(grid):
+    r = len(grid)
+    dp = [[-1] * r for _ in range(r + 1)]
+
+    def solve(i, j):
+        if i == r:
+            return grid[i][j]
+        
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        down = grid[i][j] + solve(i + 1, j)
+        diagonal = grid[i][j] + solve(i + 1, j + 1)
+        dp[i][j] = min(down, diagonal)
+        return dp[i][j]
+
+# tabulation with space optimization
+
+    def tabulation():
+        prev = grid[r-1]
+        for i in range(r-2,-1,-1):
+            curr = [-1]*r
+            for j in range(0,i+1):
+                down = grid[i][j] + prev[j]
+                diagonal = grid[i][j] + prev[j + 1]
+                curr[j] = min(down, diagonal)
+            prev = curr
+        return prev[0]
+    
+    return tabulation()
+
+ans = minimumPathSumInTriangular([[2],[3,4],[6,5,7],[4,1,8,3]])
+print(ans)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,31 @@
-def uniquePaths(obstacleGrid):
-    m = len(obstacleGrid)
-    n = len(obstacleGrid[0])
-    prev = [0]*n
-    for i in range(m):
-        curr=[0]*n
-        for j in range(n):
-            if i == 0 and j == 0 and obstacleGrid[i][j]!=1:
-                curr[j] = 1
-            else:
-                up = prev[j] if i > 0 and obstacleGrid[i][j] != 1 else 0
-                left = curr[j - 1] if j > 0 and obstacleGrid[i][j] != 1 else 0
-                curr[j] = up + left
-        prev = curr
-    return prev[n-1]
-ans = uniquePaths([[0,0,0],[0,1,0],[0,0,0]])
+def minimumPathSumInTriangular(grid):
+    r = len(grid)
+    dp = [[-1] * r for _ in range(r + 1)]
+
+    def solve(i, j):
+        if i == r:
+            return grid[i][j]
+        
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        down = grid[i][j] + solve(i + 1, j)
+        diagonal = grid[i][j] + solve(i + 1, j + 1)
+        dp[i][j] = min(down, diagonal)
+        return dp[i][j]
+
+    def tabulation():
+        prev = grid[r-1]
+        for i in range(r-2,-1,-1):
+            curr = [-1]*r
+            for j in range(0,i+1):
+                down = grid[i][j] + prev[j]
+                diagonal = grid[i][j] + prev[j + 1]
+                curr[j] = min(down, diagonal)
+            prev = curr
+        return prev[0]
+    
+    return tabulation()
+
+ans = minimumPathSumInTriangular([[2],[3,4],[6,5,7],[4,1,8,3]])
 print(ans)

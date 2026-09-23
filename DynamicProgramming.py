24592ae090da0fdb327ebdc768e166343107755e
RaunaKSharma-err <progritik@gmail.com>
2026-09-23 21:20:18 +0545

    init

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -274,3 +274,35 @@ def reverseDegreeOfString(val):
 ans = reverseDegreeOfString("abc")
 print(ans)
 
+def PartitionEqualSubsetSum(nums , target):
+    def Recursion(index,total):
+        if index >= len(nums):  
+            return target == total            
+        pick = Recursion(index + 1, total+ nums[index])
+        notPick = Recursion(index + 1, total)
+        return pick or notPick
+
+    dp = [[False for _ in range(len(nums))]for _ in range(target)]
+    def Memoization(index,total):
+        if total == 0:
+            return True
+        if index == 0:
+            if nums[0] == total:
+                return True
+        if dp[index][total] != False:
+            return True
+        if nums[index] > total:
+            pick = False
+        else:
+            pick = Memoization(index + 1, total+ nums[index])
+        notPick = Memoization(index + 1, total)
+        dp[index][total] =  pick or notPick
+        return dp[index][total]
+
+    def Tabulation():
+        pass
+    return Memoization(0,0)
+
+nums = [1,5,11,5]
+target = sum(nums)//2
+print(PartitionEqualSubsetSum(nums,target))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,37 +1,32 @@
-def cherryPickup(grid):
-    r = len(grid)
-    c = len(grid[0])
-    dp = [[[-1 for _ in range(c)]for _ in range(c)]for _ in range(r)]
-    def Recursion(i, j1, j2):
-        if j1 < 0 or j1 >= c or j2 < 0 or j2 >= c:
-            return float("-inf")
-        
-        if i == r - 1:
-            if j1 == j2:
-                return grid[i][j1]
-            return grid[i][j1] + grid[i][j2]
-        
-        if dp[i][j1][j2]!=-1:
-            return dp[i][j1][j2]
-        
-        if j1 == j2:
-            current = grid[i][j1]
+def PartitionEqualSubsetSum(nums , target):
+    def Recursion(index,total):
+        if index >= len(nums):  
+            return target == total            
+        pick = Recursion(index + 1, total+ nums[index])
+        notPick = Recursion(index + 1, total)
+        return pick or notPick
+
+    dp = [[False for _ in range(len(nums))]for _ in range(target)]
+    def Memoization(index,total):
+        if total == 0:
+            return True
+        if index == 0:
+            if nums[0] == total:
+                return True
+        if dp[index][total] != False:
+            return True
+        if nums[index] > total:
+            pick = False
         else:
-            current = grid[i][j1] + grid[i][j2]
+            pick = Memoization(index + 1, total+ nums[index])
+        notPick = Memoization(index + 1, total)
+        dp[index][total] =  pick or notPick
+        return dp[index][total]
 
-        maxi = float("-inf")
+    def Tabulation():
+        pass
+    return Memoization(0,0)
 
-        for new_j1 in range(-1, 2):
-            for new_j2 in range(-1, 2):
-                ans = current + Recursion(
-                    i + 1,
-                    j1 + new_j1,
-                    j2 + new_j2
-                )
-                maxi = max(maxi, ans)
-        dp[i][j1][j2] = maxi
-        return dp[i][j1][j2]
-    return Recursion(0, 0, c - 1)
-
-ans = cherryPickup([[3,1,1],[2,5,1],[1,5,5],[2,1,1]])
-print(ans)
+nums = [1,5,11,5]
+target = sum(nums)//2
+print(PartitionEqualSubsetSum(nums,target))

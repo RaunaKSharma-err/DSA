39a97bd432c03e233e4146f1b1235b6cb25d4d68
RaunaKSharma-err <progritik@gmail.com>
2026-09-24 20:06:07 +0545

    minoperation leetcode solution

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -306,3 +306,20 @@ def PartitionEqualSubsetSum(nums , target):
 nums = [1,5,11,5]
 target = sum(nums)//2
 print(PartitionEqualSubsetSum(nums,target))
+
+from collections import deque
+def minOperation(nums,x):
+    count =0
+    num = deque(nums)
+    while x!=0:
+        if x >= num[0] >= num[-1]:
+            x -=num[0]
+            num.popleft()
+        else:
+            x-= num[-1]
+            num.pop()
+        count+=1
+    return count
+ans = minOperation([1,1,4,2,3],5)
+print(ans)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,32 +1,15 @@
-def PartitionEqualSubsetSum(nums , target):
-    def Recursion(index,total):
-        if index >= len(nums):  
-            return target == total            
-        pick = Recursion(index + 1, total+ nums[index])
-        notPick = Recursion(index + 1, total)
-        return pick or notPick
-
-    dp = [[False for _ in range(len(nums))]for _ in range(target)]
-    def Memoization(index,total):
-        if total == 0:
-            return True
-        if index == 0:
-            if nums[0] == total:
-                return True
-        if dp[index][total] != False:
-            return True
-        if nums[index] > total:
-            pick = False
+from collections import deque
+def minOperation(nums,x):
+    count =0
+    num = deque(nums)
+    while x!=0:
+        if x >= num[0] >= num[-1]:
+            x -=num[0]
+            num.popleft()
         else:
-            pick = Memoization(index + 1, total+ nums[index])
-        notPick = Memoization(index + 1, total)
-        dp[index][total] =  pick or notPick
-        return dp[index][total]
-
-    def Tabulation():
-        pass
-    return Memoization(0,0)
-
-nums = [1,5,11,5]
-target = sum(nums)//2
-print(PartitionEqualSubsetSum(nums,target))
+            x-= num[-1]
+            num.pop()
+        count+=1
+    return count
+ans = minOperation([1,1,4,2,3],5)
+print(ans)
